2ffc9e8c4ad8fcd90731dc6d2fc0990c3a298544
TORO <altynbektorobaev91@gmail.com>
2022-11-20 18:04:40 +0600

    Added some sql files

---
+++ Altynbek/sql-assignment/expressionsVerification.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE solution()
+BEGIN
+	SELECT
+	id,
+	a, 
+	b, 
+	operation, 
+	c
+	FROM expressions
+	WHERE case WHEN operation = '+' THEN a + b
+				WHEN operation = '-' THEN a - b
+				WHEN operation = '/' THEN a / b
+				WHEN operation = '*' THEN a * b
+	END = c
+	ORDER BY id;
+END
