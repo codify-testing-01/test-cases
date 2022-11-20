a3d5b7264a983976de961838a981b11a1ae1b3b2
Altynbek Torobaev <106395728+toro-550@users.noreply.github.com>
2022-11-20 22:08:22 +0600

    Update expressionsVerification.sql

--- Altynbek/sql-assignment/expressionsVerification.sql
+++ Altynbek/sql-assignment/expressionsVerification.sql
@@ -4,9 +4,9 @@ BEGIN
 	id, a, b, operation, c
 	FROM expressions
 	WHERE case WHEN operation = '+' THEN a + b
-				WHEN operation = '-' THEN a - b
-				WHEN operation = '/' THEN a / b
-				WHEN operation = '*' THEN a * b
+	           WHEN operation = '-' THEN a - b
+		   WHEN operation = '/' THEN a / b
+		   WHEN operation = '*' THEN a * b
 	END = c
 	ORDER BY id;
 END
