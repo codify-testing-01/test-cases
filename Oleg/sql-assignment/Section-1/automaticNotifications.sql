529d603aaf6e0f479c6f80dc3f66cd578208f496
Mechanic-S1-4-1 <48251220+Mechanic-S1-4-1@users.noreply.github.com>
2022-11-22 20:09:55 +0700

    Update automaticNotifications.sql

--- Oleg/sql-assignment/Section-1/automaticNotifications.sql
+++ Oleg/sql-assignment/Section-1/automaticNotifications.sql
@@ -1,5 +1,2 @@
 CREATE PROCEDURE solution()
-    SELECT email
-    FROM users
-    WHERE role NOT IN ("admin", "premium")
-    ORDER BY email;
+    SELECT email FROM users WHERE role NOT IN ("admin", "premium") ORDER BY email;
