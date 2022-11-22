0ebac22b1cdfa4ea1d6413dfe2dc6fa8fb2e8542
Mechanic-S1-4-1 <48251220+Mechanic-S1-4-1@users.noreply.github.com>
2022-11-22 20:17:25 +0700

    Update expressionsVerification.sql

--- Oleg/sql-assignment/Section-3/expressionsVerification.sql
+++ Oleg/sql-assignment/Section-3/expressionsVerification.sql
@@ -1,12 +1,10 @@
 
 CREATE PROCEDURE solution()
 BEGIN
-    SELECT *
-    FROM expressions
-    WHERE (operation = '+' AND a+b = c)
-        OR (operation = '-' AND a-b = c)
-        OR (operation = '*' AND a*b = c)
-        OR (operation = '/' AND a/b = c)
-    ORDER BY id;
-	
+SELECT * FROM expressions
+WHERE (operation = '+' AND a+b = c)
+   OR (operation = '-' AND a-b = c)
+   OR (operation = '*' AND a*b = c)
+   OR (operation = '/' AND a/b = c)
+ORDER BY id;
 END
