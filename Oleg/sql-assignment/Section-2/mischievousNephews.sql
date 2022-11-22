ac31cf8e0a19aef92d34c4344e9c4534201034f5
Mechanic-S1-4-1 <48251220+Mechanic-S1-4-1@users.noreply.github.com>
2022-11-22 20:13:50 +0700

    Update mischievousNephews.sql

--- Oleg/sql-assignment/Section-2/mischievousNephews.sql
+++ Oleg/sql-assignment/Section-2/mischievousNephews.sql
@@ -1,6 +1,5 @@
 CREATE PROCEDURE solution()
 BEGIN
-	SELECT WEEKDAY(mischief_date) as weekday, mischief_date, author, title
-    FROM mischief
-    ORDER BY weekday, FIELD(author,"Huey", "Dewey","Louie"), mischief_date, title;
+SELECT WEEKDAY(mischief_date) as weekday, mischief_date, author, title FROM mischief
+ORDER BY weekday, FIELD(author,"Huey", "Dewey","Louie"), mischief_date, title;
 END
