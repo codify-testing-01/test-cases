070ce1cea7a4e820de8ce32840220756987a6c9b
Mechanic-S1-4-1 <48251220+Mechanic-S1-4-1@users.noreply.github.com>
2022-11-22 20:19:39 +0700

    Update securityBreach.sql

--- Oleg/sql-assignment/Section-3/securityBreach.sql
+++ Oleg/sql-assignment/Section-3/securityBreach.sql
@@ -1,7 +1,5 @@
 CREATE PROCEDURE solution()
 BEGIN
-SELECT first_name, second_name, attribute
-FROM users
-WHERE BINARY attribute LIKE  CONCAT('_%', CONCAT('\%',first_name, '\_',second_name,'\%'), '%');
-	
+SELECT first_name, second_name, attribute FROM users WHERE BINARY attribute 
+LIKE  CONCAT('_%', CONCAT('\%',first_name, '\_',second_name,'\%'), '%');
 END
