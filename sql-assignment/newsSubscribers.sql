249ff89fcadf63caaabddd677260319ac4bb70b5
Sultanmurat <112296873+QA-Sam@users.noreply.github.com>
2022-11-23 19:59:06 +0600

    Done

--- sql-assignment/newsSubscribers.sql
+++ sql-assignment/newsSubscribers.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE solution()
 BEGIN
-	SELECT subscriber FROM full_year WHERE newspaper LIKE '%daily%'
-    UNION (SELECT subscriber FROM half_year WHERE newspaper LIKE '%daily%')
-    ORDER BY subscriber;
+SELECT subscriber FROM full_year WHERE newspaper LIKE '%daily%'
+UNION (SELECT subscriber FROM half_year WHERE newspaper LIKE '%daily%')
+ORDER BY subscriber;
 END
