fc86e9204a43b2307b9dcbb4ffc40acf705f6d55
Oleg Matuschenko <777kipa@mail.ru>
2022-11-24 15:06:50 +0700

    sql-assignment

---
+++ Oleg_M/Section-2/contestLeaderboard.sql
@@ -0,0 +1,4 @@
+CREATE PROCEDURE solution()
+BEGIN
+SELECT name FROM leaderboard ORDER By score DESC LIMIT 3,5;
+END
---
+++ Oleg_M/Section-3/expressionsVerification.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE solution()
+BEGIN
+SELECT * FROM expressions
+ WHERE (operation = '+' AND a+b = c)
+    OR (operation = '-' AND a-b = c)
+    OR (operation = '*' AND a*b = c)
+    OR (operation = '/' AND a/b = c)
+ ORDER BY id;
+END
