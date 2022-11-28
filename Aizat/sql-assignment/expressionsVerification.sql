e420da5a8290efd1c68b0d382db7bbad30e0ffd1
aizat-b <aizat-b@mail.ru>
2022-11-28 20:38:51 +0600

    edited text

--- Aizat/sql-assignment/expressionsVerification.sql
+++ Aizat/sql-assignment/expressionsVerification.sql
@@ -1,8 +1,8 @@
-CREATE PROCEDURE solution()
-BEGIN
+    CREATE PROCEDURE solution()
+    BEGIN
     SELECT * FROM expressions
     WHERE (operation = '+' && c = a+b)
     || (operation = '-' && c = a-b)
     || (operation = '/' && c = a/b)
     || (operation = '*' && c = a*b);
-END
+    END
