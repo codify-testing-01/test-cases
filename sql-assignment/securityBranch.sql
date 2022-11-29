35f3793cda8423dba2e4dcee0ce6d3e5fbbe031d
Sultanmurat <112296873+QA-Sam@users.noreply.github.com>
2022-11-29 08:50:40 +0600

    Update securityBranch.sql

--- sql-assignment/securityBranch.sql
+++ sql-assignment/securityBranch.sql
@@ -1,8 +1,7 @@
 CREATE PROCEDURE solution()
 BEGIN
-	SELECT *
-	FROM users
-	WHERE attribute LIKE Binary CONCAT ('%_\%%',
-	first_name, '\_', second_name, '%\%%')
-	ORDER BY attribute;
+SELECT *
+FROM users
+WHERE attribute LIKE Binary CONCAT ('%_\%%', first_name, '\_', second_name, '%\%%')
+ORDER BY attribute;
 END
